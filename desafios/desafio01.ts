b0ff1661ead4955cfb2a78f4cba6dd4fc02fbf2e
GilvaniaMoreira <moreiragil84@gmail.com>
2022-06-10 02:59:52 -0300

    first commit

---
+++ desafios/desafio01.ts
@@ -0,0 +1,30 @@
+/* Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+*/
+
+// A way to solve
+const funcionario = {
+    codigo: 10,
+    nome: "Jhon"
+}
+
+// Other
+interface employee {
+    cod: number,
+    name: string
+}
+
+let employee1: employee = {
+    cod: 200,
+    name: "Jhon"
+}
+
+
+// Other
+const funcionario2: {codigo: number, nome: string} = {
+    codigo: 200,
+    nome: "Jhon"
+}
